68e17eea6878f1d1f4f0febe9e29079763792c79
Regina Reynolds <rhreynolds@hotmail.co.uk>
2025-08-12 13:13:22 +0100

    feat: add check for experment id

---
+++ hooks/pre_gen_project.py
@@ -0,0 +1,20 @@
+import os
+import re
+import tempfile
+
+VALID_ID = r"[a-z]{2}\d+"
+
+
+def check_experiment_id(experiment_id: str) -> None:
+    assert bool(
+        re.fullmatch(VALID_ID, experiment_id)
+    ), f"Invalid experiment_id supplied: {experiment_id}"
+
+
+def is_cruft_updating() -> bool:
+    return tempfile.gettempdir() in os.getcwd()
+
+
+if __name__ == "__main__":
+    if not is_cruft_updating():
+        check_experiment_id("{{ cookiecutter.experiment_id }}")
