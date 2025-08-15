2dc25f1cde5b7eac8023dbaba3881d873b657945
Regina Reynolds <rhreynolds@hotmail.co.uk>
2025-08-15 16:34:46 +0100

    feat: add notebooks

---
+++ {% if cookiecutter.experiment_id %}{{cookiecutter.experiment_id}}-{{cookiecutter.__project}}{% else %}{{cookiecutter.__project}}{% endif %}/notebooks/01-template_analysis.py
@@ -0,0 +1,63 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.1
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # Title
+#
+# ## Aim
+#
+# -
+#
+# ## Background
+#
+# -
+#
+# ## Methods
+#
+# -
+#
+# ## Load modules
+
+# %% [markdown]
+
+import matplotlib.pyplot as plt
+
+# %%
+import session_info
+
+# %% [markdown]
+# ## Package versions
+
+# %%
+session_info.show()
+
+# %% [markdown]
+# ## Results
+#
+# ### Text
+#
+# - Add any observations about results/figures here.
+# - For figures, add descriptive text and interpretation.
+
+# %% [markdown]
+# ### Figures
+
+# %%
+plt.plot([1, 23, 2, 4])
+plt.show()
+
+# %% [markdown]
+# ## Conclusions
+#
+# -
